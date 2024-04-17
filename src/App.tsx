ff657346882baa5be01f0b257acaa53bda1be395
ishara <ishara9@gmail.com>
2024-04-17 16:05:04 +0530

    Updated objects inside array

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,49 @@
 import { useState } from "react";
 import "./App.css";
-import NavBar from "./components/NavBar";
-import Cart from "./components/Cart";
+import Button from "./components/Button/Button";
 
 function App() {
-  const [cartItems, setCartItems] = useState([
-    "Product1",
-    "Product2",
-    "Product3",
-  ]);
+  const [game, setGame] = useState({
+    id: 1,
+    player: {
+      name: "Player 1",
+      score: 0,
+    },
+    tags: [
+      { id: 1, name: "Sports", quanity: 10 },
+      { id: 2, name: "Run", quanity: 20 },
+    ],
+  });
+
+  function handleClick(): void {
+    setGame({
+      ...game,
+      player: {
+        ...game.player,
+        name: "Bob",
+        score: game.player.score + 1,
+      },
+      tags: game.tags.map((tag) =>
+        tag.id === 1 ? { ...tag, quanity: tag.quanity + 1 } : tag
+      ),
+    });
+  }
 
   return (
     <>
-      <div>app</div>
-      <NavBar cartItemsCount={cartItems.length}></NavBar>
-      <Cart cartItems={cartItems} onClear={() => setCartItems([])}></Cart>
+      <h1>Player Name: {game.player.name}</h1>
+      <h2>Score: {game.player.score}</h2>
+      <h3>Tags</h3>
+      <ul>
+        {game.tags.map((tag) => (
+          <li key={tag.id}>
+            {tag.name} - {tag.quanity}
+          </li>
+        ))}
+      </ul>
+      <Button onClick={() => handleClick()}>
+        <h1>Update Score!</h1>
+      </Button>
     </>
   );
 }
