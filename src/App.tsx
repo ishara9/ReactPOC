b78ba2bff807b020ead864429908da6d49610080
ishara <ishara9@gmail.com>
2024-04-17 15:42:39 +0530

    Added NavBar and Cart and clear cart

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,20 @@
 import { useState } from "react";
-import Alert from "./components/Alert";
-import Button from "./components/Button/Button";
-import ListGroup from "./components/ListGroup";
 import "./App.css";
-import LikeButton from "./components/LikeButton/LikeButton";
-import Message from "./components/Message";
-import Bugs from "./components/Bugs";
+import NavBar from "./components/NavBar";
+import Cart from "./components/Cart";
 
 function App() {
-  // const [alertVisible, setAlertVisible] = useState(false);
-  // const [like, setLike] = useState(false);
+  const [cartItems, setCartItems] = useState([
+    "Product1",
+    "Product2",
+    "Product3",
+  ]);
 
   return (
     <>
-      {/* {alertVisible && (
-        <div>
-          <Alert text="Hello" onClose={() => setAlertVisible(false)}>
-            Child Prop<h1> rock </h1>
-          </Alert>
-        </div>
-      )} */}
-      {/* <div>
-        <ListGroup></ListGroup>
-      </div> */}
-      {/* <div>
-        <Button color="primary" onClick={() => setAlertVisible(true)}>
-          Custom Button
-        </Button>
-      </div> */}
-      {/* <LikeButton onClick={() => console.log("liked")} /> */}
-      {/* <Message /> */}
-      <Bugs />
+      <div>app</div>
+      <NavBar cartItemsCount={cartItems.length}></NavBar>
+      <Cart cartItems={cartItems} onClear={() => setCartItems([])}></Cart>
     </>
   );
 }
