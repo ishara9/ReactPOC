2f0e60350073ee99c817b915915ecdadc048643b
ishara <ishara9@gmail.com>
2024-05-29 16:09:03 +0530

    custom hook for effect hook useUsers

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,33 +1,8 @@
-import apiClient from "./api-client";
+import create from "./HttpService";
 
 export interface User {
     id: number;
     name: string;
 }
 
-class UserService {
-
-    getAllUsers() {
-        const controller = new AbortController();
-        const request = apiClient
-            .get<User[]>("/users", {
-                signal: controller.signal,
-            })
-        return { request, cancel: () => controller.abort() }
-    }
-
-    deletUser(id: number) {
-        return apiClient.delete("/users/" + id)
-    }
-
-    createUser(newUser: any) {
-        return apiClient
-            .post("/users", newUser)
-    }
-
-    updateUser(user: User) {
-        return apiClient.patch("/users/" + user.id, user);
-    }
-}
-
-export default new UserService();
+export default create("/users");
